fa0549652a73e276c5f0d7b66dc1cd247cff5a90
donghyeon <jdhyeon280812@gmail.com>
2026-02-24 17:23:46 +0900

    Modify CMakeLists for PolyOps.td and errors

--- lib/Dialect/Poly/PolyOps.h
+++ lib/Dialect/Poly/PolyOps.h
@@ -1,11 +1,11 @@
 #ifndef LIB_DIALECT_POLY_POLYOPS_H_
 #define LIB_DIALECT_POLY_POLYOPS_H_
 
-#include "lib/Dialect/Poly/PolyDialect.h"
-#include "lib/Dialect/Poly/PolyTypes.h"
-#include "mlir/include/mlir/IR/BuiltinOps.h"    // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypes.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/Dialect.h"       // from @llvm-project
+#include "PolyDialect.h"
+#include "PolyTypes.h"
+#include "mlir/IR/BuiltinOps.h"    // from @llvm-project
+#include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
+#include "mlir/IR/Dialect.h"       // from @llvm-project
 
 #define GET_OP_CLASSES
 #include "PolyOps.h.inc"
