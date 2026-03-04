5179c3e022d7448ba5052c82e7fc145b1494600a
donghyeon <jdhyeon280812@gmail.com>
2026-03-04 10:53:08 +0900

    Add SameOperandsAndResultType This removes the flexibility of having mixed poly + tensor ops for the binary operations, but demonstrates how the type inference engine enables a more succinct textual IR.
    If you were to simplify the assembly format without doing this, you'd
    get a compile-time error complaining that it can't infer the type of the
    operands or argument.

--- lib/Dialect/Poly/PolyOps.h
+++ lib/Dialect/Poly/PolyOps.h
@@ -6,6 +6,7 @@
 #include "mlir/IR/BuiltinOps.h"    // from @llvm-project
 #include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
 #include "mlir/IR/Dialect.h"       // from @llvm-project
+#include "mlir/Interfaces/InferTypeOpInterface.h" // from @llvm-project
 
 #define GET_OP_CLASSES
 #include "PolyOps.h.inc"
