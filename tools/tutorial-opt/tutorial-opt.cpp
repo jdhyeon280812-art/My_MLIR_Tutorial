44c32ea1a324068fb227b0c0899219e87878e373
donghyeon <jdhyeon280812@gmail.com>
2026-03-10 12:02:25 +0900

    register PolyToStandard passes to tutorial-opt

--- tools/tutorial-opt/tutorial-opt.cpp
+++ tools/tutorial-opt/tutorial-opt.cpp
@@ -1,6 +1,7 @@
 #include "Transform/Affine/Passes.h"
 #include "Dialect/Poly/PolyDialect.h"
 #include "Transform/Arith/Passes.h"
+#include "Conversion/PolyToStandard/PolyToStandard.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
@@ -21,6 +22,9 @@ int main(int argc, char **argv) {
   // ✅ 패스 등록 (이 한 줄이면 Passes.h에 정의된 모든 패스가 한 번에 등록됩니다!)
     mlir::tutorial::registerAffineFullUnroll();
     mlir::tutorial::registerAffineFullUnrollPatternRewrite();
+
+  // Dialect conversion passes
+  mlir::tutorial::poly::registerPolyToStandardPass();
 
   // 메인 엔진(MlirOptMain) 실행 -> 파일 읽기, 파싱, 에러 처리, 결과 출력 등 컴파일러의 복잡한 뒷단 작업을 MLIR 프레임워크에게 위임. 우리는 패스만 만들어서 등록하면 됨
   return mlir::asMainReturnCode(
